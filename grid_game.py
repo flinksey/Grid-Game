559e8dcef5f25fcf4b800bcdad158133f3cd58af
Ramona Rajagopalan <65932912+flinksey@users.noreply.github.com>
2020-08-20 22:37:55 +0800

    Updated grid_game.py
    Created a dict with tuples containing cell coordinates as keys and the displayed number in each cell as the values

--- grid_game.py
+++ grid_game.py
@@ -1,19 +1,26 @@
 import random as rdm
 
-def randomizer(k):
-  #Generate random numbers within range to be displayed in the cells
-  num = rdm.randint(0,K-1) #will double-check with old notes about this
-  return num
-
 def set_up():
-  #Get input from user: M, N, K
-  M = int(input("Please enter no. of rows: "))
-  N = int(input("Please enter no. of cols: "))
-  K = int(input("Please enter boundary num: "))
-  print("\n")
+  M = int(input("no. of rows: "))
+  N = int(input("no. of cols: "))
+  K = int(input("boundary no: "))
 
-  #How can I generate the grid?
-  dim = M*N   #number of cells
-  grid_list = [] #unsure of whether I'll keep this but 
+  nums = [rdm.randint(0,K-1) for i in range(M*N)] #nums displayed in each cell of the grid
+  kys = [(a,b) for a in range(M) for b in range(N)] #tuples representing coords of each cell
+  #print to double-check
+  print(nums) 
+  print(kys)
 
+  #initially, used a list but wanted to see if using a dict would make it easier to select & manipulate a cell given the coords from User
+  grid_vals = {kys[_]: nums[_] for _ in range(M*N)}
+  print(grid_vals)
+  #print(len(grid_vals))
+  
+  #works up to here---------------|
+  #figuring out how to actually generate the grid
+
+  for row in range(M):
+      for col in range(N):
+          print()
+    
 set_up()
