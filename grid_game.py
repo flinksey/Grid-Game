7033a657b7178ad8b1ec6a0e9f536f62cde89777
Ramona Rajagopalan <65932912+flinksey@users.noreply.github.com>
2020-08-21 23:40:24 +0800

    Update grid_game.py
    Now able to generate grid BUT problem is that the grid will have to be updated based on changes to elements in the cell list so I'll have to find a way of generating the grid from the cell list instead of the other way around

--- grid_game.py
+++ grid_game.py
@@ -1,26 +1,40 @@
-import random as rdm
+# grid game but using a list instead of a dict for the grid content
+import random
+
+def rand_gen(K):
+    num = random.randint(0,K-1)
+    return num
+
+def start_game(M, N, inside):
+    #Problem: I'm creating the cell content list AS the grid is being generated, but
+    #I'll have to find a way to generate the updated grid based on the updated list
+    
+    return 0
 
 def set_up():
-  M = int(input("no. of rows: "))
-  N = int(input("no. of cols: "))
-  K = int(input("boundary no: "))
-
-  nums = [rdm.randint(0,K-1) for i in range(M*N)] #nums displayed in each cell of the grid
-  kys = [(a,b) for a in range(M) for b in range(N)] #tuples representing coords of each cell
-  #print to double-check
-  print(nums) 
-  print(kys)
-
-  #initially, used a list but wanted to see if using a dict would make it easier to select & manipulate a cell given the coords from User
-  grid_vals = {kys[_]: nums[_] for _ in range(M*N)}
-  print(grid_vals)
-  #print(len(grid_vals))
-  
-  #works up to here---------------|
-  #figuring out how to actually generate the grid
-
-  for row in range(M):
-      for col in range(N):
-          print()
+    M = int(input("no. of rows: "))
+    N = int(input("no. of cols: "))
+    K = int(input("boundary no: "))
     
+    cell = [] #format is [no. displayed in cell, row, col]
+    row_label = [j for j in range(M)]
+    col_label = [i for i in range(N)]
+    row_label.insert(0, " ")
+
+    print("\nLet's start!\n")
+    
+    for row in range(M+1):
+        print(row_label[row], end="  ")
+        if row == 0:
+            for col in range(N):
+                print(col_label[col], end="   ")
+        else:
+            for col in range(N):
+                ins = rand_gen(K)
+                print(ins, end=" | ")
+                cell.append([ins,row-1,col])
+        print(" ")
+    
+    #start_game(M,N,cell)
+
 set_up()
